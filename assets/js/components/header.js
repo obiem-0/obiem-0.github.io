10bb047be75e6c67b726a3aec128b59c4929c5c1
Obi E <117234245+obiem-0@users.noreply.github.com>
2023-09-14 15:14:03 +0100

    Update header.js

--- assets/js/components/header.js
+++ assets/js/components/header.js
@@ -11,7 +11,7 @@ class Head extends HTMLElement {
         <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Chukwubuikem Emuwa</title>
+        <title>Obi's Portfolio</title>
         <link rel="shortcut icon" type="image/png" href="../assets/images/logos/favicon.png" />
         <link rel="stylesheet" href="../assets/css/styles.min.css" />
 
