bf316ceac98c702be078f4d3f5ce5fb9c798f4c0
Obi Emuwa <emuwa.obi@gmail.com>
2023-11-01 18:54:33 +0000

    Changes to be committed: 	modified:   src/layouts/full/footer/Footer.js

--- src/layouts/full/footer/Footer.js
+++ src/layouts/full/footer/Footer.js
@@ -17,8 +17,8 @@ const Footer = (props) => {
 
 <h4 class="mb-0 px-4 fs-4"> Reach out to me on <span class="mx-3"> </span>
 <a href="https://www.linkedin.com/in/obi-emuwa" target="_blank" class="pe-1 text-primary text-decoration-underline"> Linkedin</a> |
-<a href="https://discordapp.com/users/emuwa.obi" target="_blank" class="pe-1 text-primary text-decoration-underline"> Discord</a> |
 <a href="mailto:emuwa.obi@gmail.com" class="pe-1 text-primary text-decoration-underline"> Email</a> |
+<a href="https://discordapp.com/users/emuwa.obi" target="_blank" class="pe-1 text-primary text-decoration-underline"> Discord</a> |
 <a href="https://github.com/obiem" target="_blank" class="pe-1 text-primary text-decoration-underline"> Github</a>
 
 </h4>
